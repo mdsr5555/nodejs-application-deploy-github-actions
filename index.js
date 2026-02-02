c4f4c59ec89a6e24dd35f89daab19a214db4c555
Mudassar Sarfraz <sahil_k_mudassar@yahoo.com>
2026-02-02 18:18:59 +0000

    3rd commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8080
 
 app.get('/', (req, res) => {
-    return res.json({msg: 'Hello from server ${PORT}'})
+    return res.json({msg: 'Hello from server ubuntu'})
 })
 
 app.listen(PORT, () => {
